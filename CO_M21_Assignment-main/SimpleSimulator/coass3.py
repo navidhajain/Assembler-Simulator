b82de61068d78f1ab90f9d3001c1d4a0f980af38
Navidha Jain <navidha20223@iiitd.ac.in>
2021-08-23 17:06:37 +0530

    updated

--- CO_M21_Assignment-main/SimpleSimulator/coass3.py
+++ CO_M21_Assignment-main/SimpleSimulator/coass3.py
@@ -1,25 +1,31 @@
 import matplotlib.pyplot as plt
 
-regs= {"000": "0000000000000000",
-       "001": "0000000000000000",
-       "010": "0000000000000000",
-       "011": "0000000000000000",
-       "100": "0000000000000000",
-       "101": "0000000000000000",
-       "110": "0000000000000000",
-       "111": "0000000000000000"}
+regs = {"000": "0000000000000000",
+        "001": "0000000000000000",
+        "010": "0000000000000000",
+        "011": "0000000000000000",
+        "100": "0000000000000000",
+        "101": "0000000000000000",
+        "110": "0000000000000000",
+        "111": "0000000000000000"}
 
 memory_address = []
+program_counter = 0
+cycle_number = 0
+x = []
+y = []
+
 
 def getbin(value):
     v = int(value)
-    if value>=0:
+    if value >= 0:
         x = bin(v).replace("0b", "")
     else:
-        x=bin(v).replace("-0b", "")
+        x = bin(v).replace("-0b", "")
     if len(x) < 16:
         x = "0" * (16 - len(x)) + str(x)
     return str(x)
+
 
 def getpc(value):
     v = int(value)
@@ -28,40 +34,44 @@ def getpc(value):
         x = "0" * (8 - len(x)) + str(x)
     return str(x)
 
+
 def getint(string):
     return int(string, 2)
 
-def add(line): #INCOMPLETE
+
+def add(line):  
     global program_counter
     reg1 = line[7:10]
     reg2 = getint(regs[line[10:13]])
     reg3 = getint(regs[line[13:]])
-    reg4 = reg2+reg3
+    reg4 = reg2 + reg3
 
-    if reg4>256 or reg4<0:
-        regs["111"] = "0000000000001000"        
-    else:        
+    if reg4 > 256 or reg4 < 0:
+        regs["111"] = "0000000000001000"
+    else:
         regs["111"] = "0000000000000000"
 
-    regs[reg1]=getbin(reg4)    
+    regs[reg1] = getbin(reg4)
     printline(line)
-    program_counter += 1    
+    program_counter += 1
 
-def sub(line): #INCOMPLETE
+
+def sub(line): 
     global program_counter
     reg1 = line[7:10]
     reg2 = getint(regs[line[10:13]])
     reg3 = getint(regs[line[13:]])
     reg4 = reg2 - reg3
 
-    if reg4 > 256 or reg4< 0:
-        regs["111"] = "0000000000001000"       
-    else:        
+    if reg4 > 256 or reg4 < 0:
+        regs["111"] = "0000000000001000"
+    else:
         regs["111"] = "0000000000000000"
 
-    regs[reg1] = getbin(reg4)    
+    regs[reg1] = getbin(reg4)
     printline(line)
     program_counter += 1
+
 
 def movimm(line):
     global program_counter
@@ -78,6 +88,7 @@ def movreg(line):
     printline(line)
     program_counter += 1
 
+
 def load(line):
     global program_counter
     value = getint(line[8:])
@@ -85,6 +96,7 @@ def load(line):
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
+
 
 def store(line):
     global program_counter
@@ -94,7 +106,8 @@ def store(line):
     printline(line)
     program_counter += 1
 
-def multiply(line): #INOMPLETE
+
+def multiply(line):  
     global program_counter
     reg1 = line[7:10]
     reg2 = getint(regs[line[10:13]])
@@ -102,23 +115,21 @@ def multiply(line): #INOMPLETE
     reg4 = reg2 * reg3
 
     if reg4 > 256 or reg4 < -256:
-        regs["111"] = "0000000000001000"        
-    else:     
+        regs["111"] = "0000000000001000"
+    else:
         regs["111"] = "0000000000000000"
 
-    regs[reg1] = getbin(reg4)            
+    regs[reg1] = getbin(reg4)
     printline(line)
-    program_counter += 1    
+    program_counter += 1
 
-#nwkna
-#nsakv
 
 def divide(line):
     global program_counter
-    reg1=getint(line[10:13])
-    reg2=getint(line[13:])
-    regs["000"]=getbin(reg1/reg2)
-    regs["001"]=getbin(reg1%reg2)
+    reg1 = getint(line[10:13])
+    reg2 = getint(line[13:])
+    regs["000"] = getbin(reg1 / reg2)
+    regs["001"] = getbin(reg1 % reg2)
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
@@ -126,10 +137,10 @@ def divide(line):
 
 def rightshift(line):
     global program_counter
-    regused=line[5:8]
-    a=getint(regs[regused])
-    imm= getint(line[8:])
-    regs[regused]=getbin(a>>imm)
+    regused = line[5:8]
+    a = getint(regs[regused])
+    imm = getint(line[8:])
+    regs[regused] = getbin(a >> imm)
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
@@ -137,10 +148,10 @@ def rightshift(line):
 
 def leftshift(line):
     global program_counter
-    regused=line[5:8]
-    a=getint(regs[regused])
-    imm= getint(line[8:])
-    regs[regused]=getbin(a<<imm)
+    regused = line[5:8]
+    a = getint(regs[regused])
+    imm = getint(line[8:])
+    regs[regused] = getbin(a << imm)
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
@@ -148,10 +159,10 @@ def leftshift(line):
 
 def xor(line):
     global program_counter
-    reg1=line[7:10]
-    reg2=getint(line[10:13])
-    reg3 =getint(line[13:])
-    regs[reg1]=getbin(reg2^reg3)
+    reg1 = line[7:10]
+    reg2 = getint(line[10:13])
+    reg3 = getint(line[13:])
+    regs[reg1] = getbin(reg2 ^ reg3)
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
@@ -159,10 +170,10 @@ def xor(line):
 
 def or1(line):
     global program_counter
-    reg1=line[7:10]
-    reg2=getint(line[10:13])
-    reg3 =getint(line[13:])
-    regs[reg1]=getbin(reg2|reg3)
+    reg1 = line[7:10]
+    reg2 = getint(line[10:13])
+    reg3 = getint(line[13:])
+    regs[reg1] = getbin(reg2 | reg3)
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
@@ -170,10 +181,10 @@ def or1(line):
 
 def and1(line):
     global program_counter
-    reg1=line[7:10]
-    reg2=getint(line[10:13])
-    reg3 =getint(line[13:])
-    regs[reg1]=getbin(reg2 & reg3)
+    reg1 = line[7:10]
+    reg2 = getint(line[10:13])
+    reg3 = getint(line[13:])
+    regs[reg1] = getbin(reg2 & reg3)
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
@@ -181,12 +192,13 @@ def and1(line):
 
 def invert(line):
     global program_counter
-    reg1=line[10:13]
-    reg2 =getint(line[13:])
-    regs[reg1]=getbin(~reg2)
+    reg1 = line[10:13]
+    reg2 = getint(line[13:])
+    regs[reg1] = getbin(~reg2)
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
+
 
 def compare(line):
     global program_counter
@@ -201,21 +213,24 @@ def compare(line):
     printline(line)
     program_counter += 1
 
+
 def unconjump(line):
     global program_counter
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter = getint(line[8:])
 
+
 def jlt(line):
     global program_counter
-    if regs["111"] == "0000000000000100" :
+    if regs["111"] == "0000000000000100":
         printline(line)
         program_counter = getint(line[8:])
         return
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
+
 
 def jgt(line):
     global program_counter
@@ -227,6 +242,7 @@ def jgt(line):
     printline(line)
     program_counter += 1
 
+
 def je(line):
     global program_counter
     if regs["111"] == "0000000000000001":
@@ -237,128 +253,110 @@ def je(line):
     printline(line)
     program_counter += 1
 
+
 def hlt(line):
     global program_counter
     regs["111"] = "0000000000000000"
     printline(line)
     program_counter += 1
 
-program_counter=0
-#isko yahan pe likhne se kuch nhi hoga isse acha upar hi likh dete top pe
+
 def printline(line):
     print(getpc(program_counter), end=' ')
     for i in regs:
         print(regs[i], end=' ')
     print()
 
+
 def process(line):
     global program_counter
+    global x
+    global y
+    global cycle_number
 
     code = line[:5]
     if code == "00000":
         add(line)
-        #printline(line)
     if code == "00001":
         sub(line)
-        #printline(line)
     if code == "00010":
         movimm(line)
-        #printline(line)
     if code == "00011":
         movreg(line)
-        #printline(line)
-    if code=="00100":
-        load(line)        
-        #printline(line)
-    if code=="00101":
+    if code == "00100":
+        load(line)
+        x.append(cycle_number)
+        y.append(getint(line[8:]))
+        
+    if code == "00101":
         store(line)
-        #printline(line)
+        x.append(cycle_number)
+        y.append(getint(line[8:]))
+        
     if code == "00111":
         multiply(line)
-        #printline(line)
-    if code=="00111":
+    if code == "00111":
         divide(line)
-        #printline(line)
-    if code=="01000":
+    if code == "01000":
         rightshift(line)
-        #printline(line)
-    if code=="01001":
+    if code == "01001":
         leftshift(line)
-        #printline(line)
-    if code=="01010":
+    if code == "01010":
         xor(line)
-        #printline(line)
-    if code=="01011":
+    if code == "01011":
         or1(line)
-       # printline(line)
-    if code=="01100":
+    if code == "01100":
         and1(line)
-        #printline(line)
-    if code=="01101":
+    if code == "01101":
         invert(line)
-        #printline(line)
-    if code=="01110":
+    if code == "01110":
         compare(line)
-        #printline(line)
-    if code=="01111":
+    if code == "01111":
         unconjump(line)
-       # printline(line)
-    if code=="10000":
+    if code == "10000":
         jlt(line)
-        #printline(line)
-    if code=="10001":
+    if code == "10001":
         jgt(line)
-        #printline(line)
-    if code=="10010":
+    if code == "10010":
         je(line)
-        #printline(line)
-    if code=="10011":
+    if code == "10011":
         hlt(line)
-        #printline(line)
 
-x = []
-y = []
 
 def main():
     global memory_address
-    cycle_number=0
+    global cycle_number
     global program_counter
+    global x
+    global y
     arr = []
-    
     while True:
         try:
             line = input()
-            if line == "ss":
-                break
             arr.append(line)
         except EOFError:
             break
     len_arr = len(arr)
-    for i in range(0,256):
+    for i in range(0, 256):
         if i < len_arr:
             memory_address.append(arr[i])
         if i >= len_arr:
             memory_address.append("0000000000000000")
 
-
-    while program_counter >=0 and program_counter < len(arr):
+    while program_counter >= 0 and program_counter < len(arr):
         x.append(cycle_number)
         y.append(program_counter)
         process(arr[program_counter])
-        cycle_number+=1
-        
-    # print("yash is god")
-    # print(x)
-    # print(y)
-    plt.plot(x,y,'o')
-    plt.savefig("abc.png")
-    #if plt.show doesnt work use plt.savefig(specify path wjere pu want to store the png)
-    #for load and store 2 points aane chahiye cuz we are accenss from 2 sides
+        cycle_number += 1
 
-
+    plt.xlabel("Cycle Number")
+    plt.ylabel("Memory Address")
+    plt.plot(x, y, 'o')
+    plt.show()
+    
     for i in range(len(memory_address)):
-        print(memory_address[i], end = '\n')
+        print(memory_address[i], end='\n')
 
-if __name__== "__main__":
-    #print("yash is god")
+
+if __name__ == "__main__":
     main()
